c8b241f5dc88f1ec822eceaa48f313dd6d1b5b97
Jakub Urbanowski <kubau3344@gmail.com>
2026-03-18 11:38:17 +0100

    Remove TextArea export from main.ts
    Removed export of TextArea component from main.ts.
    
    Signed-off-by: Jakub Urbanowski <kubau3344@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./index.css";
 
-export * from "./components/ui/Badge/Badge";
 export { Button, buttonVariants } from "./components/ui/Button/Button";
 export { Checkbox } from "./components/ui/Checkbox/Checkbox";
 export {
@@ -11,4 +10,3 @@ export { Modal } from "./components/ui/Modal/Modal";
 export { RadioGroup } from "./components/ui/RadioGroup/RadioGroup";
 export { RadioGroupItem } from "./components/ui/RadioGroup/RadioGroupItem/RadioGroupItem";
 export { Switch } from "./components/ui/Switch/Switch";
-export { TextArea } from "./components/ui/TextArea/TextArea";
