ff12979819e4af9fa934d59336166248f85b4003
rudywasd <gameinluki@gmail.com>
2026-03-20 12:47:41 +0100

    fix: changed Radio Group styles

--- src/components/ui/RadioGroup/RadioGroup.tsx
+++ src/components/ui/RadioGroup/RadioGroup.tsx
@@ -1,19 +1,27 @@
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
-function RadioGroup({
-  className,
-  ...props
-}: React.ComponentProps<typeof RadioGroupPrimitive.Root>) {
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, orientation = "vertical", ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
       data-slot="radio-group"
-      className={cn("grid gap-2", className)}
+      orientation={orientation}
+      className={cn(
+        "grid gap-2",
+        // Logic to handle visual orientation
+        orientation === "horizontal" ? "grid-flow-col auto-cols-max" : "grid-cols-1",
+        className
+      )}
+      {...ref}
       {...props}
     />
   );
-}
+});
+
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
 
 export { RadioGroup };
