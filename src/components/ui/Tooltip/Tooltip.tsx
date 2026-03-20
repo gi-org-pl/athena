4e6af578f703f164f881de2aa2d4cd6dfd815c52
Jakub Urbanowski <kubau3344@gmail.com>
2026-03-20 13:01:49 +0100

    feat: add tooltip component

---
+++ src/components/ui/Tooltip/Tooltip.tsx
@@ -0,0 +1,47 @@
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import * as React from "react";
+import {
+  Tooltip as ShadcnTooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip-base";
+
+export interface TooltipProps {
+  children: React.ReactNode;
+  content: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+  delay?: number;
+  dataTestId?: string;
+}
+
+const Tooltip = ({
+  children,
+  content,
+  side = "top",
+  align = "center",
+  delay = 200,
+  dataTestId,
+}: TooltipProps) => {
+  return (
+    <TooltipProvider delayDuration={delay}>
+      <ShadcnTooltip>
+        <TooltipTrigger asChild>
+          <span className="cursor-default inline-block">{children}</span>
+        </TooltipTrigger>
+        <TooltipContent
+          side={side}
+          align={align}
+          data-test-id={dataTestId}
+          className="bg-[#003c47] text-white border-none shadow-md"
+        >
+          {content}
+          <TooltipPrimitive.Arrow className="fill-[#003c47]" />
+        </TooltipContent>
+      </ShadcnTooltip>
+    </TooltipProvider>
+  );
+};
+
+export { Tooltip };
